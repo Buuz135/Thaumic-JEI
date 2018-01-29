fa55bb3813017752a917166e015d63cb4b55831c
Buuz135 <buuz135@hotmail.com>
2018-01-29 23:17:07 +0100

    Updated descriptions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,4 @@
 + Aspects are now ingredients, they can be searched en JEI, see what 2 aspects make another aspect and what items create that aspect.
 + Arcane Workbench has a clickable area to check recipes and you can transfer recipes from the items in your inventory.
 + Fixed some recipes not working
++ WARNING: Checking what aspects all the items in the game has can be performance heavy but it won't add load time to the pack,, everything is done in a new thread and it might take a while to appear in game. Example: All The Mods 1.10 takes 3 min to check the aspects of all the items
--- src/main/java/com/buuz135/thaumicjei/config/ThaumicConfig.java
+++ src/main/java/com/buuz135/thaumicjei/config/ThaumicConfig.java
@@ -6,6 +6,6 @@ import net.minecraftforge.common.config.Config;
 @Config(modid = ThaumicJEI.MOD_ID)
 public class ThaumicConfig {
 
-    @Config.Comment("Allow the scanning of all items in game to show what items can make aspects. WARNING: This can be performance heavy, everything is done in a new thread and it might take a while to appear in game!")
+    @Config.Comment("Allow the scanning of all items in game to show what items can make aspects. WARNING: This can be performance heavy but it won't add load time to the pack, everything is done in a new thread and it might take a while to appear in game!")
     public static boolean enableAspectFromItemStacks = true;
 }
