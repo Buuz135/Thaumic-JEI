fad8f6d6d09de914ce0549c9029b8be47f4d1007
Buuz135 <buuz135@hotmail.com>
2018-03-11 01:24:41 +0100

    Revert "add Option to disable Threading"
    This reverts commit 8a76f86

--- src/main/java/com/buuz135/thaumicjei/config/ThaumicConfig.java
+++ src/main/java/com/buuz135/thaumicjei/config/ThaumicConfig.java
@@ -6,8 +6,6 @@ import net.minecraftforge.common.config.Config;
 @Config(modid = ThaumicJEI.MOD_ID)
 public class ThaumicConfig {
 
-    @Config.Comment("Allow the scanning of all items in game to show what items can make aspects. This can be performance heavy.")
+    @Config.Comment("Allow the scanning of all items in game to show what items can make aspects. WARNING: This can be performance heavy but it won't add load time to the pack, everything is done in a new thread and it might take a while to appear in game!")
     public static boolean enableAspectFromItemStacks = true;
-    @Config.Comment("Enabling this switch adds load time to the pack by diabling threading for AspectFromItemStacks but makes sure everything is loaded from the beginning and prevents a (theoretically) possible bug. If you leave this switch disabled everything will be done in a new thread and might take a while to appear in game.")
-    public static boolean disableThreading = false;
 }
