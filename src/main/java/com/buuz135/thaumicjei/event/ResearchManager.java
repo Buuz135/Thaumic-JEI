6cd75d0863c11e5176295a93c5795b3a02c18afd
Buuz135 <buuz135@hotmail.com>
2018-08-05 22:00:37 +0200

    Delay recipe syncing if the player is null, closes #30

--- src/main/java/com/buuz135/thaumicjei/event/ResearchManager.java
+++ src/main/java/com/buuz135/thaumicjei/event/ResearchManager.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of Hot or Not.
+ *
+ * Copyright 2018, Buuz135
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.buuz135.thaumicjei.event;
 
 import com.buuz135.thaumicjei.ThaumcraftJEIPlugin;
@@ -19,7 +40,7 @@ import thaumcraft.api.research.ResearchEvent;
 @Mod.EventBusSubscriber(modid = ThaumicJEI.MOD_ID, value = Side.CLIENT)
 public class ResearchManager {
 
-    private static int needSync = -1;
+    private static int timeToSync = -1;
 
     @SubscribeEvent
     public static void onResearch(ResearchEvent.Research event) {
@@ -29,20 +50,24 @@ public class ResearchManager {
     @SubscribeEvent
     public static void onJoin(WorldEvent.Load event) {
         if (event.getWorld() instanceof WorldClient) {
-            needSync = 20;
+            timeToSync = 20;
         }
     }
 
     @SubscribeEvent
     public static void onTick(TickEvent.ClientTickEvent event) {
-        if (needSync >= 0 && event.phase == TickEvent.Phase.END) {
-            if (needSync == 0) sync("");
-            needSync--;
+        if (timeToSync >= 0 && event.phase == TickEvent.Phase.END) {
+            if (timeToSync == 0) sync("");
+            timeToSync--;
         }
     }
 
     private static void sync(String current) {
         if (!ThaumicConfig.hideRecipesIfMissingResearch) return;
+        if (Minecraft.getMinecraft().player == null) {
+            timeToSync = 20;
+            return;
+        }
         if (Minecraft.getMinecraft().player.hasCapability(ThaumcraftCapabilities.KNOWLEDGE, null)) {
             for (String uuid : new String[]{ThaumcraftJEIPlugin.arcaneWorkbenchCategory.getUid(), ThaumcraftJEIPlugin.crucibleCategory.getUid(), ThaumcraftJEIPlugin.infusionCategory.getUid()}) {
                 ThaumcraftJEIPlugin.runtime.getRecipeRegistry().getRecipeWrappers(ThaumcraftJEIPlugin.runtime.getRecipeRegistry().getRecipeCategory(uuid)).forEach(o -> ThaumcraftJEIPlugin.runtime.getRecipeRegistry().hideRecipe((IRecipeWrapper) o, uuid));
